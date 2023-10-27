ca90018f54fee90276c93bdbeafe68a51c7dc315
9ziggy9 <davidarogers@protonmail.com>
2023-10-27 14:08:49 -0700

    address caching successfully implemented

--- src/geo.ts
+++ src/geo.ts
@@ -7,12 +7,25 @@ export const ROSEVILLE_COORD: Coord = [-121.2880,38.7521];
 const urlNominatimSearch = (addr: string): string =>
   `https://nominatim.openstreetmap.org/search?format=json&q=${addr}`;
 
+async function checkGeoCache(addr: string): Promise<[Coord, Coord] | null> {
+  const cachedLotLan = localStorage.getItem(addr);
+  return cachedLotLan
+    ? JSON.parse(cachedLotLan)
+    : null;
+}
+
 // Consider changing to a structured query which accepts a UrlSearchParms obj.
 async function geocodeAddr(addr: string): Promise<[Coord, Coord] | null> {
+  const cachedData = await checkGeoCache(addr);
+  if (cachedData) {
+    console.log(`Cached data found: ${addr}`);
+    return cachedData;
+  }
   const res = await fetch(urlNominatimSearch(addr+",Roseville,CA"));
   const data = await res.json();
   if (data.length) {
     const [{lon, lat}] = data;
+    localStorage.setItem(addr, JSON.stringify([lon, lat]));
     return [lon, lat];
   }
   return null;
