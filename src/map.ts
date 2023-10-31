6ad03ac2fba59308c6a193b044f152671f88d3b6
9ziggy9 <davidarogers@protonmail.com>
2023-10-31 14:48:23 -0700

    adding on click events for opening info pane

--- src/map.ts
+++ src/map.ts
@@ -11,27 +11,20 @@ import Point from "ol/geom/Point";
 import Feature from "ol/Feature";
 import {Icon, Style, Fill, Stroke, Circle} from "ol/style";
 import {Select} from "ol/interaction";
-import {pointerMove} from "ol/events/condition";
+import {pointerMove, click} from "ol/events/condition";
 import * as olCoord from "ol/coordinate";
 // END OpenLayer
 
 const RADIUS_PIN = 6;
-enum PinColor {
-    Base03 = '#002B36',  // Background
-    Base02 = '#073642',  // Content background
-    Base01 = '#586E75',  // Comments/Secondary content
-    Base00 = '#657B83',  // Body text/default code/primary content
-    Base0  = '#839496',  // Optional emphasized content
-    Base1  = '#93A1A1',  // Optional emphasized content (brighter)
-    Yellow = '#B58900',
-    Orange = '#CB4B16',
-    Red    = '#DC322F',
-    Magenta= '#D33682',
-    Violet = '#6C71C4',
-    Blue   = '#268BD2',
-    Cyan   = '#2AA198',
-    Green  = '#859900'
-}
+const PinColors = [
+  "green",
+  "purple",
+  "red",
+  "orange",
+  "yellow",
+  "blue",
+  "cyan",
+];
 
 export const newMap = (centerCoord: olCoord.Coordinate): Map => new Map({
   controls: [],
@@ -49,17 +42,18 @@ export const newMap = (centerCoord: olCoord.Coordinate): Map => new Map({
 
 export function pinAllData(map: Map, table: CitationTable): void {
   console.log("Following table available:", table);
-  for (const insp of Object.keys(table)) {
+  Object.keys(table).forEach((insp, i) => {
     for (const {id, latlon} of table[insp]) {
-      if (latlon) addCirclePin(map, latlon, id);
+      if (latlon) addCirclePin(map, latlon, id, PinColors[i % PinColors.length]);
     }
-  }
+  });
 }
 
 export function addCirclePin(
   map: Map,
   coords: olCoord.Coordinate,
   id: string,
+  pinColor: string,
 ): void {
   const pinFeature = new Feature({
     geometry: new Point(projection.fromLonLat(coords))
@@ -68,10 +62,10 @@ export function addCirclePin(
     image: new Circle({
       radius: RADIUS_PIN,
       fill: new Fill({
-        color: PinColor.Red, // reimplement with color coded by inspector
+        color: pinColor, // reimplement with color coded by inspector
       }),
       stroke: new Stroke({
-        color: PinColor.Base00,
+        color: "black",
         width: 2,
       })
     }),
@@ -91,8 +85,20 @@ export function addCirclePin(
     layers: [vectorLayer]
   });
 
+  const selectClick = new Select({
+    condition: click,
+    layers: [vectorLayer]
+  });
+
   map.addLayer(vectorLayer);
   map.addInteraction(selectHover);
+  map.addInteraction(selectClick);
+
+  selectClick.on("select", e => {
+    if (e.selected.length > 0) {
+      console.log(`Clicked on ${e.selected[0].getId()}`)
+    }
+  });
 
   selectHover.on("select", e => {
     if (e.selected.length > 0) {
